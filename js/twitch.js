a03036a2e3996793cf7afb2b5ffbe71769c1a8d3
Jeay <heyj3ay@gmail.com>
2025-02-27 20:55:03 +1100

    Update twitch.js

--- js/twitch.js
+++ js/twitch.js
@@ -1,39 +1,42 @@
 const streamers = ['penta', 'shroud', 'dgthe99', 'teegrizzley', 'prawln', 'richopov'];
 
 async function checkLiveStreamers() {
-    const clientId = 'gp762nuuoqcoxypju8c569th9wz7q5';
-    const accessToken = 'g8h3swjn3jsz531hgvb6auian7ep4a';
+    const clientId = 'kimne78kx3ncx6brgo4mv6wki5h1ko'; // Using public Twitch web client ID
     const headers = { 
-        'Client-ID': clientId, 
-        'Authorization': `Bearer ${accessToken}`
+        'Client-ID': clientId,
+        'Content-Type': 'application/json'
     };
 
     try {
         // Get stream info
-        const response = await fetch(`https://api.twitch.tv/helix/streams?user_login=${streamers.join('&user_login=')}`, { headers });
+        const response = await fetch(`https://api.twitch.tv/helix/streams?user_login=${streamers.join('&user_login=')}`, { 
+            headers: {
+                'Client-ID': 'gp762nuuoqcoxypju8c569th9wz7q5',
+                'Authorization': `Bearer g8h3swjn3jsz531hgvb6auian7ep4a`
+            }
+        });
         const data = await response.json();
         
         if (data.data && data.data.length > 0) {
-            // Get stream access token
-            const tokenResponse = await fetch(`https://gql.twitch.tv/gql`, {
+            // Get stream access token using GQL
+            const gqlResponse = await fetch('https://gql.twitch.tv/gql', {
                 method: 'POST',
-                headers: {
-                    'Client-ID': 'kimne78kx3ncx6brgo4mv6wki5h1ko',
-                    'Content-Type': 'application/json'
-                },
+                headers,
                 body: JSON.stringify({
-                    query: `{
-                        streamPlaybackAccessToken(channelName: "${data.data[0].user_login}", params: {platform: "web", playerType: "site"}) {
+                    query: `
+                    {
+                        streamPlaybackAccessToken(channelName: "${data.data[0].user_login}", params: {platform: "web", playerType: "embed"}) {
                             value
                             signature
                         }
-                    }`
+                    }
+                    `
                 })
             });
-            const tokenData = await tokenResponse.json();
+            const tokenData = await gqlResponse.json();
             
-            // Get HLS URL
-            const hlsUrl = `https://usher.ttvnw.net/api/channel/hls/${data.data[0].user_login}.m3u8?client_id=${clientId}&token=${tokenData.data.streamPlaybackAccessToken.value}&sig=${tokenData.data.streamPlaybackAccessToken.signature}`;
+            // Get HLS URL with valid token
+            const hlsUrl = `https://usher.ttvnw.net/api/channel/hls/${data.data[0].user_login}.m3u8?client_id=${clientId}&token=${encodeURIComponent(tokenData.data.streamPlaybackAccessToken.value)}&sig=${tokenData.data.streamPlaybackAccessToken.signature}&allow_source=true`;
             return hlsUrl;
         }
         return null;
